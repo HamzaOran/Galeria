901d3567a44a127de7ea51efb990fc0581824817
mardinian <60844909+mardinian@users.noreply.github.com>
2023-11-24 09:04:20 +0300

    fix ImageResults ımport

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,5 +1,5 @@
 import fetchImages from '@/lib/fetchImages';
-import type { ImagesResults } from '@/models/Images';
+import type { ImageResults } from '@/models/Images';
 import ImgContainer from './ImgContainer';
 import addBlurredDataUrls from '@/lib/getBase64';
 
@@ -12,7 +12,7 @@ export default async function Gallery({ topic }: Props) {
     ? 'https://api.pexels.com/v1/curated'
     : `https://api.pexels.com/v1/search?query=${topic}`;
 
-  const images: ImagesResults | undefined = await fetchImages(url);
+  const images: ImageResults | undefined = await fetchImages(url);
 
   if (!images)
     return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>;
