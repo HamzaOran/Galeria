2bff83d90d9ba77a374d17ac7e0983306626278c
mardinian <60844909+mardinian@users.noreply.github.com>
2023-11-24 09:41:40 +0300

    add loading page

---
+++ src/app/results/[term]/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center mt-96">
+      <div className="border-2 p-4 border-black border-dashed rounded-xl text-lg">
+        Loading...
+      </div>
+    </div>
+  );
+}
